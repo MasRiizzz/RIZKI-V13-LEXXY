cc150d93f20e3f619b5d4094aa1eff69d77450c2
MasRiizzz-Md <106753250+MasRiizzz@users.noreply.github.com>
2022-06-10 13:26:19 +0700

    Update ind.js

--- recode/ind.js
+++ recode/ind.js
@@ -1,8 +1,8 @@
 exports.paymentstore = (owner) => {
 return `*PAYMENT STORE*
-Dana : 0881-7864-684
-Ovo : 0823-1646-4080
-Pulsa : 0823-1646-4080
+Dana : 0858-7681-0596
+Ovo : 0858-7681-0596
+Pulsa : 0858-7681-0596
 
 Note :
 Jika Sudah Transfer Wajib Kirim
